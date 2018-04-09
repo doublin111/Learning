f1207b112cefd2787db4c07d578f39ffdb7196e9
doublin <doublinandroid@gmail.com>
2018-04-09 18:57:24 +0200

    new

--- src/Playground1/Program4/Game.java
+++ src/Playground1/Program4/Game.java
@@ -5,17 +5,34 @@ public class Game extends Ranks {
     final static int MAX_HAND_SIZE = 5;
 
     public static void main(String[] args) {
-
         Deck deck = new Deck();
         deck.shuffle();
 
         Hand firstPlayer = new Hand();
 
+        /* manually created cards used to test hand ranks methods */
+
+//        Card card1 = new Card(1, 2);
+//        Card card2 = new Card(2, 2);
+//        Card card3 = new Card(3, 2);
+//        Card card4 = new Card(4, 2);
+//        Card card5 = new Card(5, 2);
+
+//        Hand rankTestHand = new Hand();
+
+//        rankTestHand.addCard(card1);
+//        rankTestHand.addCard(card2);
+//        rankTestHand.addCard(card3);
+//        rankTestHand.addCard(card4);
+//        rankTestHand.addCard(card5);
+
+        /* end testings */
+
         for (int i = 0; i < MAX_HAND_SIZE; i++) {
             firstPlayer.addCard(deck.dealCard());
         }
 
-        firstPlayer.sortByValue();
+        // firstPlayer.sortByValue();
 
         for (int i = 0; i < MAX_HAND_SIZE; i++) {
             System.out.print(firstPlayer.getCard(i).toString());
--- src/Playground1/Program4/Ranks.java
+++ src/Playground1/Program4/Ranks.java
@@ -4,7 +4,11 @@ public class Ranks {
 
     static void checkHandRanks(Hand hand) {
 
-        if (isThreeOfAKind(hand) > 3.0)
+        if (isFlush(hand) > 5.0)
+            System.out.println("You got a flush.");
+        else if (isStraight(hand) > 4.0)
+            System.out.println("You have a straight.");
+        else if (isThreeOfAKind(hand) > 3.0)
             System.out.println("You have three of a kind.");
         else if (isTwoPair(hand) > 2.0)
             System.out.println("You have two pairs.");
@@ -14,14 +18,39 @@ public class Ranks {
     }
 
 
-    /*********************** Methods for hand rankings. *********************************
+    /************************ Methods for hand rankings. ********************************
      **** It returns a double (ex. 2.0 for two pairs or 3.0 for three of a kind) ********
      **** plus a decimal which represents the high card(s) when two or more *************
      **** players have the same hands. **************************************************
      ***********************************************************************************/
 
+    static double isFlush(Hand hand) {
+        double result = 0.0;
+        String suit = hand.getCard(0).getSuitAsString();
+
+        for (int i = 0; i < 4; i++) {
+            result = 5.0 + (hand.getCard(i).getValue() * 0.01);
+            if (!hand.getCard(i).getSuitAsString().equals(suit))
+                result = 0.0;
+        }
+        return result;
+    }
+
+    static double isStraight(Hand hand) {
+        double result = 0.0;
+        hand.sortByValue();
+
+        for (int i = 0; i < 4; i++) {
+            if (hand.getCard(i).getValue() == hand.getCard(i + 1).getValue() - 1) {
+                result = 4.0 + (hand.getCard(i + 1).getValue() * 0.01);
+            } else result = 0.0;
+        }
+        return result;
+    }
+
     static double isThreeOfAKind(Hand hand) {
         double result = 0.0;
+        hand.sortByValue();
 
         for (int i = 0; i < 3; i++) {
             if (hand.getCard(i).getValue() == hand.getCard(i + 1).getValue() && hand.getCard(i).getValue() == hand.getCard(i + 2).getValue()) {
